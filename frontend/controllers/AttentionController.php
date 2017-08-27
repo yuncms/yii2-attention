0d6ae4df31f3bdadb6f0b105749270be5aae7093
Chao Xu <xutongle@gmail.com>
2017-08-27 15:38:07 +0800

    cc

--- frontend/controllers/AttentionController.php
+++ frontend/controllers/AttentionController.php
@@ -120,30 +120,4 @@ class AttentionController extends Controller
         }
         return ['status' => 'followed'];
     }
-
-    /**
-     * 关注某tag
-     * @return array
-     * @throws NotFoundHttpException
-     */
-    public function actionTag()
-    {
-        Yii::$app->response->format = Response::FORMAT_JSON;
-        $modelId = Yii::$app->request->post('model_id', null);
-        $source = Tag::findOne($modelId);
-        if (!$source) {
-            throw new NotFoundHttpException ();
-        }
-        /** @var \yuncms\user\models\User $user */
-        $user = Yii::$app->user->identity;
-        if ($user->hasTagValues($source->id)) {
-            $user->removeTagValues($source->id);
-            $user->save();
-            return ['status' => 'unfollowed'];
-        } else {
-            $user->addTagValues($source->id);
-            $user->save();
-            return ['status' => 'followed'];
-        }
-    }
 }
