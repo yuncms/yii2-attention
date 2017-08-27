7096a4a521054e97f2cded5e4465197bd453fa51
Chao Xu <xutongle@gmail.com>
2017-08-27 12:13:27 +0800

    init

---
+++ Module.php
@@ -0,0 +1,38 @@
+<?php
+
+
+namespace yuncms\attention;
+
+
+use Yii;
+
+/**
+ * Class Module
+ * @package yuncms\credit
+ */
+class Module extends \yii\base\Module
+{
+    /**
+     * 初始化
+     */
+    public function init()
+    {
+        parent::init();
+        $this->registerTranslations();
+    }
+
+    /**
+     * 注册语言包
+     * @return void
+     */
+    public function registerTranslations()
+    {
+        if (!isset(Yii::$app->i18n->translations['attention*'])) {
+            Yii::$app->i18n->translations['attention*'] = [
+                'class' => 'yii\i18n\PhpMessageSource',
+                'sourceLanguage' => 'en-US',
+                'basePath' => __DIR__ . '/messages',
+            ];
+        }
+    }
+}
